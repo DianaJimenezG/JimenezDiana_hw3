a70a6bea48aaa7036a52de96da62606dfc6f7d03
Diana Jimenez <dm.jimenez10@uniandes.edu.co>
2018-10-24 20:47:51 -0500

    imagen filtrada con pasa bajas

--- Fourier2D.py
+++ Fourier2D.py
@@ -18,3 +18,25 @@ plt.imshow(np.abs(np.real(im_fft_2)), norm=LogNorm(vmin=5))
 plt.colorbar()
 plt.title('Transformada de Fourier')
 plt.show()
+
+def filtro(f):
+    for i in range(f.shape[0]):
+        for j in range(f.shape[1]):
+            if f[i,j]>=10**3.5:
+                f[i,j]=(f[i+1,j]+f[i-1,j]+f[i,j+1]+f[i,j-1])/4.0
+    return f
+
+img_filt=filtro(im_fft)
+
+plt.figure(3)
+plt.imshow(np.abs(np.real(img_filt)), norm=LogNorm(vmin=5))
+plt.colorbar()
+plt.title('Transformada de Fourier con Filtro')
+plt.show()
+
+imagen=fftpack.ifft2(img_filt).real
+
+plt.figure(4)
+plt.imshow(imagen, plt.cm.gray)
+plt.title('Imagen filtrada')
+plt.show()
