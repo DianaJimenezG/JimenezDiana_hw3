549c986680e413c04d817a3e72354bd9c6519034
Diana Jimenez <dm.jimenez10@uniandes.edu.co>
2018-10-25 21:25:59 -0500

    matriz de covarianza verificada

--- PCR.py
+++ PCR.py
@@ -13,32 +13,35 @@ for i in range(diagn.shape[0]):
     if diagn[i]=='B':
         D[i]=0
 
-
 ######### Funciones ##########
 #Funcion que normalizacion la matriz de variables.
 #Para cada columna, resta el promedio y divide por la varianza.
 def V_norm(M):
     for j in range(M.shape[1]):
         v=M[:,j]
-        prom=np.average(v)
+        p=np.average(v)
         var=np.var(v)
-        M[:,j]=(v-prom)/np.sqrt(var)
+        M[:,j]=(v-p)/np.sqrt(var)
     return M
 
 Vn=V_norm(np.copy(V))
-print np.average(V[:,0])
-print np.average(Vn[:,0])
-print np.var(V[:,0])
-print np.var(Vn[:,0])
 
+def cov_ij(V1,V2):
+    x=np.empty(0)
+    mV1=np.mean(V1)
+    mV2=np.mean(V2)
+    for k in range(V1.size):
+        x=np.append(x,((V1[k]-mV1)*(V2[k]-mV2))/(V1.shape[0]-1))
+    return np.sum(x)
 
-#def m_cov(dT1,dT2,dT3,dT4):
-#   var=np.array(([dT1],[dT2],[dT3],[dT4]))
-#   M=np.zeros(shape=(4,4))
-#   for j in range(4):
-#      for i in range(4):
-#         M[i,j]=sigma_ij(var[i,:],var[j,:])
-#   return M
+def m_cov(Mn):
+    n=Mn.shape[1]
+    C=np.zeros(shape=(n,n))
+    for j in range(n):
+        for i in range(n):
+            C[i,j]=cov_ij(Mn[:,i],Mn[:,j])
+    return C
 
-
-#print m_cov(dT1,dT2,dT3,dT4)
+Mc=m_cov(Vn)
+m=np.cov(np.transpose(Vn))
+print np.allclose(Mc,m)
