b4aaf8db657da6717e25ada36177ed8930218976
Diana Jimenez <dm.jimenez10@uniandes.edu.co>
2018-10-27 23:25:32 -0500

    fourier funciona

--- Fourier.py
+++ Fourier.py
@@ -6,27 +6,20 @@ datos=np.genfromtxt("signal.dat", delimiter=",")
 t=datos[:,0]
 y=datos[:,1]
 
-#d_incompletos=np.genfromtxt("incompletos.dat", delimiter=",")
-#t_inc=datos[:,0]
-#y_inc=datos[:,1]
+d_incompletos=np.genfromtxt("incompletos.dat", delimiter=",")
+t_inc=datos[:,0]
+y_inc=datos[:,1]
 
 N=y.shape[0]
 
 def transf_fourier(y, N):
-    F=np.zeros(N)+0j
+    F = np.zeros(N) + 0j
+    n = np.arange(N)
     for k in range(N):
-        r=0.0
-        i=0j
-        for n in range(N):
-            a=y[n]*np.exp(-1j*2.0*np.pi*k*(n/N))
-            r+=np.real(a)
-            i+=np.imag(a)
-        F[k]=r+i
+            F[k] = np.sum(y*np.exp(-1j*2.0*np.pi*k*n/N))
     return F
 
 freq=np.fft.fftfreq(N, t[1]-t[0])
-print transf_fourier(y, N)
-
 
 plt.figure(1)
 plt.plot(freq, np.real(transf_fourier(y, N)))
