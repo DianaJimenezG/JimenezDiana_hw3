2d6cde8b2df83bf17b27b953cb656269a3a48804
Diana Jimenez <dm.jimenez10@uniandes.edu.co>
2018-10-27 23:52:51 -0500

    bono

--- Fourier.py
+++ Fourier.py
@@ -10,8 +10,6 @@ d_incompletos=np.genfromtxt("incompletos.dat", delimiter=",")
 t_inc=datos[:,0]
 y_inc=datos[:,1]
 
-N=y.shape[0]
-
 def transf_fourier(y, N):
     F = np.zeros(N) + 0j
     n = np.arange(N)
@@ -19,7 +17,15 @@ def transf_fourier(y, N):
             F[k] = np.sum(y*np.exp(-1j*2.0*np.pi*k*n/N))
     return F
 
-freq=np.fft.fftfreq(N, t[1]-t[0])
+def frecuencias(N, dt):
+    if N%2.0 == 0:
+        frec=np.concatenate((np.linspace(0, N/2.0-1, N/2.0), np.linspace(-N/2.0, -1, N/2.0)))/(dt*N)
+    else:
+        frec=np.concatenate((np.linspace(0, (N-1)/2.0, ((N-1)/2.0)+1), np.linspace(-(N-1)/2.0, -1, (N-1)/2.0)))/(dt*N)
+    return frec
+
+N=y.shape[0]
+freq=frecuencias(N, t[1]-t[0])
 
 plt.figure(1)
 plt.plot(freq, np.real(transf_fourier(y, N)))
