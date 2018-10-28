97625f7d04b8aa990621b3b3634397d86608d89c
Diana Jimenez <dm.jimenez10@uniandes.edu.co>
2018-10-28 08:26:50 -0500

    interpola

--- Fourier.py
+++ Fourier.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import interpolate
 
 ########## Datos ##########
 datos=np.genfromtxt("signal.dat", delimiter=",")
@@ -10,6 +11,7 @@ d_incompletos=np.genfromtxt("incompletos.dat", delimiter=",")
 t_inc=datos[:,0]
 y_inc=datos[:,1]
 
+########## Funciones ##########
 def transf_fourier(y, N):
     F = np.zeros(N) + 0j
     n = np.arange(N)
@@ -24,9 +26,44 @@ def frecuencias(N, dt):
         frec=np.concatenate((np.linspace(0, (N-1)/2.0, ((N-1)/2.0)+1), np.linspace(-(N-1)/2.0, -1, (N-1)/2.0)))/(dt*N)
     return frec
 
-N=y.shape[0]
-freq=frecuencias(N, t[1]-t[0])
+#Funcion que calcula la funcion de interpolacion cuadratica y cubica con la funcion interp1d de scipy.
+def interp(x, y, xi):
+    cuad=interpolate.interp1d(x, y, kind='quadratic')
+    cub=interpolate.interp1d(x, y, kind='cubic')
+    y_cuad=cuad(xi)
+    y_cub=cub(xi)
+    return y_cuad, y_cub
 
-plt.figure(1)
-plt.plot(freq, np.real(transf_fourier(y, N)))
+
+########## Calculos ##########
+N=y.shape[0]
+fou=transf_fourier(y, N)
+freq=frecuencias(N, t[1]-t[0])
+print "BONO: no se usa el paquete fftfreq para calcular las frecuencias."
+
+#Implementacion de la funcion interp para calcular la interpolacion lineal, cuadratica y cubica.
+ti=np.linspace(t_inc[1], t_inc[-1], 512)
+y_cuad=interp(t_inc, y_inc, ti)[0]
+y_cub=interp(t_inc, y_inc, ti)[1]
+Ni=ti.size
+freq_interp=frecuencias(Ni, ti[1]-ti[0])
+fou_cuad=transf_fourier(y_cuad, Ni)
+fou_cub=transf_fourier(y_cub, Ni)
+
+
+########## Graficas ##########
+g=plt.figure(1)
+plt.plot(t,y)
+plt.title('Senal original')
+plt.ylabel('y')
+plt.xlabel('Tiempo')
 plt.show()
+g.savefig('JimenezDiana_signal.pdf')
+
+h=plt.figure(2)
+plt.plot(freq, np.abs(np.real(fou)))
+plt.title('Transformada de Fourier')
+plt.ylabel('Amplitud')
+plt.xlabel('Frecuencia')
+plt.show()
+h.savefig('JimenezDiana_TF.pdf')
